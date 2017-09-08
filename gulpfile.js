1a8347ada7981ef98012c6e8b2dee7f4524e855d
dcuthell <dcc1447@gmail.com>
2017-09-08 08:51:06 -0700

    install gulp-concat, add concatInterface to gulpfile, edit jsBrowserify command in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
+const concat = require('gulp-concat');
 
 gulp.task('echo', function(){
   console.log('ECHO...echo');
@@ -11,4 +12,10 @@ gulp.task('jsBrowserify', function() {
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/pingpong-interface.js', './js/signup-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
 });
