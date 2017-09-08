cae665c564b144e0b6f320afeb6017bcb4440c2d
dcuthell <dcc1447@gmail.com>
2017-09-08 12:10:06 -0700

    add getageinyearsanddays method for Birthday class along with test that passes

--- js/galactic-calc.js
+++ js/galactic-calc.js
@@ -10,5 +10,21 @@ export class Birthday {
     return (elapsedTime / 1000);
   }
 
+  getAgeDifference(anotherBday){
+    let myAge = this.getSecondsLived();
+    let theirAge = anotherBday.getSecondsLived();
+    let ageDiff = myAge - theirAge;
+    return ageDiff;
+  }
+
+  getAgeInYearsAndDays(){
+    let seconds = this.getSecondsLived();
+    let x = seconds % 31557600;
+    let years = (seconds - x)/31557600;
+    let y = x % 86400;
+    let days = (x - y)/86400;
+    return "You are " + years + " years and " + days + " days old";
+  }
+
 
 }
--- spec/galactic-calc-spec.js
+++ spec/galactic-calc-spec.js
@@ -16,4 +16,18 @@ describe('Birthday', function(){
     expect(myBDay.getSecondsLived()).not.toEqual(sameBDay.getSecondsLived());
   })
 
+  it('should correctly return the difference between two birthdays in seconds', function(){
+    const d = new Date("October 13, 2014 11:13:00");
+    const e = new Date("October 13, 2014 11:13:01");
+    const myBDay = new Birthday(d);
+    const sameBDay = new Birthday(e);
+    expect(myBDay.getAgeDifference(sameBDay)).toEqual(1);
+  })
+
+  it('should correctly return an age in years and days', function(){
+    const d = new Date();
+    const myBDay = new Birthday(d);
+    expect(myBDay.getAgeInYearsAndDays()).toEqual('You are 0 years and 0 days old');
+  })
+
 })
