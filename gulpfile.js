11107f865843c862533040fc9f6eaa132a1de072
dcuthell <dcc1447@gmail.com>
2017-09-08 09:01:41 -0700

    install gulp-util and del, add build and clean commands to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
+const utilities = require('gulp-util');
+const buildProduction = utilities.env.production;
+const del = require('del');
+
 
 gulp.task('echo', function(){
   console.log('ECHO...echo');
@@ -25,4 +29,16 @@ gulp.task("minifyScripts", ["jsBrowserify"], function(){
   return gulp.src("./build/js/app.js")
     .pipe(uglify())
     .pipe(gulp.dest("./build/js"));
+});
+
+gulp.task("build", ['clean'], function(){
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
+});
+
+gulp.task("clean", function(){
+  return del(['build', 'tmp']);
 });
