3beb8fae24c4d243fead7ed4c68acd09c9a7cc50
dcuthell <dcc1447@gmail.com>
2017-09-08 08:45:15 -0700

    install vinyl-source-stream, add jsbrowserify command to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 const gulp = require('gulp');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
 
 gulp.task('echo', function(){
   console.log('ECHO...echo');
+});
+
+gulp.task('jsBrowserify', function() {
+  return browserify({ entries: ['./js/*.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
 });
