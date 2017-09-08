6f0a35ce98287e65501d8bff57a43feded50f4fc
dcuthell <dcc1447@gmail.com>
2017-09-08 08:57:00 -0700

    install gulp-uglify and add minifyScripts command to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,27 @@ const gulp = require('gulp');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 
 gulp.task('echo', function(){
   console.log('ECHO...echo');
 });
 
-gulp.task('jsBrowserify', function() {
-  return browserify({ entries: ['./js/*.js'] })
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({ entries: ['./tmp/allConcat.js'] })
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
 });
 
 gulp.task('concatInterface', function() {
-  return gulp.src(['./js/pingpong-interface.js', './js/signup-interface.js'])
+  return gulp.src(['./js/*-interface.js'])
     .pipe(concat('allConcat.js'))
     .pipe(gulp.dest('./tmp'));
+});
+
+gulp.task("minifyScripts", ["jsBrowserify"], function(){
+  return gulp.src("./build/js/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"));
 });
